233c988296d5e1746be566f571415baea50151c1
Abhishek Keshri <25423853+2KAbhishek@users.noreply.github.com>
2026-03-31 15:59:58 +0530

    refactor: extract isYelling func in bob kotlin

--- kotlin/bob/src/main/kotlin/Bob.kt
+++ kotlin/bob/src/main/kotlin/Bob.kt
@@ -3,17 +3,13 @@ object Bob {
         val msg = input.trimEnd()
         return when {
             msg.isBlank() -> "Fine. Be that way!"
-
-            msg.any { it.isLetter() } &&
-                    msg.all { it.isUpperCase() || !it.isLetter() } && msg.last()
-                    == '?' -> "Calm down, I know what I'm doing!"
-
-            msg.any { it.isLetter() } &&
-                    msg.all { it.isUpperCase() || !it.isLetter() } -> "Whoa, chill out!"
-
+            isYelling(msg) && msg.last() == '?' -> "Calm down, I know what I'm doing!"
+            isYelling(msg) -> "Whoa, chill out!"
             msg.last() == '?' -> "Sure."
             else -> "Whatever."
         }
     }
+
+    fun isYelling(msg: String) = msg.all { it.isUpperCase() || !it.isLetter() } && msg.any { it.isLetter() }
 }
 
